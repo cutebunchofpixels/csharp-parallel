2c0e7b2a3f3699231fa007c5a12e9fe2373931df
cutebunchofpixels <poroshkinanthony18@gmail.com>
2023-11-12 23:43:32 +0300

    Initial commit

---
+++ merge-sort/MergeSort.cs
@@ -0,0 +1,90 @@
+﻿namespace merge_sort;
+
+public class MergeSort
+{
+    private const int MinimumSubarraySize = 5000;
+    
+    private static void Merge(int[] source, int[] left, int[] right)
+    {
+        int i = 0, j = 0, k = 0;
+
+        while (i < left.Length && j < right.Length)
+        {
+            if (left[i] < right[j])
+            {
+                source[k] = left[i];
+                i++;
+            }
+            else
+            {
+                source[k] = right[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < left.Length)
+        {
+            source[k] = left[i];
+            i++;
+            k++;
+        }
+
+        while (j < right.Length)
+        {
+            source[k] = right[j];
+            j++;
+            k++;
+        }
+    }
+
+    public static void SortSerial(int[] array)
+    {
+        if (array.Length <= 1)
+        {
+            return;
+        }
+
+        var middle = array.Length / 2;
+        var left = new int[middle];
+        var right = new int[array.Length - middle];
+
+        Array.Copy(array, 0, left, 0, left.Length);
+        Array.Copy(array, middle, right, 0, right.Length);
+
+        SortSerial(left);
+        SortSerial(right);
+
+        Merge(array, left, right);
+    }
+
+    public static void SortParallel(int[] array, int depth)
+    {
+        if (array.Length <= MinimumSubarraySize)
+        {
+            SortSerial(array);
+            return;
+        }
+        
+        var middle = array.Length / 2;
+        var left = new int[middle];
+        var right = new int[array.Length - middle];
+
+        Array.Copy(array, 0, left, 0, left.Length);
+        Array.Copy(array, middle, right, 0, right.Length);
+        
+        if (depth < Environment.ProcessorCount)
+        {
+            Parallel.Invoke(
+                () => SortParallel(left, depth + 1),
+                () => SortParallel(right, depth + 1));
+        }
+        else
+        {
+            SortParallel(left, depth);
+            SortParallel(right, depth);
+        }
+        
+        Merge(array, left, right);
+    }
+}
---
+++ merge-sort/Program.cs
@@ -0,0 +1,71 @@
+﻿using System.Diagnostics;
+
+namespace merge_sort
+{
+    public static class Program
+    {
+        private const int MeasurementIterations = 10;
+        private const int ArraySize = 10000000;
+
+        public static void Main()
+        {
+            var totalSerialTime = 0L;
+            var totoalParallelTime = 0L;
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+
+            for (var i = 0; i < MeasurementIterations; i++)
+            {
+                var arraySerial = GenerateRandomArray(ArraySize);
+                var arrayParallel = GenerateRandomArray(ArraySize);
+                
+                watch.Restart();
+                MergeSort.SortSerial(arraySerial);
+                watch.Stop();
+                totalSerialTime += watch.ElapsedMilliseconds;
+
+                watch.Restart();
+                MergeSort.SortParallel(arrayParallel, 0);
+                watch.Stop();
+                totoalParallelTime += watch.ElapsedMilliseconds;
+
+                if (!IsSorted(arraySerial) || !IsSorted(arrayParallel))
+                {
+                    Console.WriteLine("Error during sorting arrays");
+                    Environment.Exit(-1);
+                }
+            }
+            
+            watch.Stop();
+            
+            Console.WriteLine($"Average serial execution time: {totalSerialTime / MeasurementIterations} ms");
+            Console.WriteLine($"Average parallel execution time: {totoalParallelTime / MeasurementIterations} ms");
+        }
+
+        private static int[] GenerateRandomArray(int length)
+        {
+            var result = new int[length];
+            var rand = new Random();
+
+            for (int i = 0; i < length; i++)
+            {
+                result[i] = rand.Next(10000);
+            }
+            
+            return result;
+        }
+
+        private static bool IsSorted(int[] array)
+        {
+            for (var i = 0; i < array.Length - 1; i++)
+            {
+                if (array[i] > array[i + 1])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }    
+}
+
